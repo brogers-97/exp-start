de047bc2199a47e62c0569ad71f1db8392d6f682
brogers-97 <brburr24@gmail.com>
2023-09-05 15:04:08 -0600

    added ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+import Landingpage from './components/Landingpage';
+import Battlefield from './components/Battlefield';
+import Homepage from './components/Homepage';
+import Decks from './components/Decks';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route
+            path='/'
+            element={<Landingpage />}
+          />
+          <Route
+            path='/battle'
+            element={<Battlefield />}
+          />
+          <Route
+            path='/home'
+            element={<Homepage />}
+          />
+          <Route
+            path='/decks'
+            element={<Decks />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
